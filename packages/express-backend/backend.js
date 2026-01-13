a3b8be71600f0d9f126cf5f53177bfe8387716fa
Pprarthana0808 <pprarthana0808@gmail.com>
2026-01-13 10:18:20 -0800

    Step5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -14,6 +14,8 @@ const findUserByName = (name) => {
     (user) => user["name"] === name
   );
 };
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 const app = express();
 const port = 8000;
 
@@ -32,7 +34,15 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
-
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
