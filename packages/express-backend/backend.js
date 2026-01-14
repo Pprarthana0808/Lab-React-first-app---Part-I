21525fa0c5f0dfb95cf2f4ae148856f750d6bd99
Pprarthana0808 <pprarthana0808@gmail.com>
2026-01-13 23:01:47 -0800

    Done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -19,11 +19,22 @@ const findUserByName = (name) => {
     (user) => user["name"] === name
   );
 };
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
+};
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 const addUser = (user) => {
   users["users_list"].push(user);
   return user;
+};
+const deleteUserById = (id) => {
+  const index = users.users_list.findIndex((user) => user.id === id);
+  if (index === -1) return false;
+  users.users_list.splice(index, 1); 
+  return true;
 };
 const app = express();
 const port = 8000;
@@ -33,19 +44,46 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.get("/users", (req, res) => {
+/* app.get("/users", (req, res) => {
  const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
 });
+*/
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+  if (name != undefined && job != undefined) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } else if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
+  const id = req.params["id"]; 
   let result = findUserById(id);
   if (result === undefined) {
     res.status(404).send("Resource not found.");
   } else {
     res.send(result);
   }
+});
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deleted = deleteUserById(id);
+
+  if (!deleted) {
+    return res.status(404).send("Resource not found.");
+  }
+
+  return res.sendStatus(200); 
 });
 app.listen(port, () => {
   console.log(
